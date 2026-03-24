236c3561f3ae12d869968c0d393e012f57991ebd
Chanul Eriagama <chanul@hectre.com>
2026-03-25 11:17:34 +1300

    test heatmap refining in refinement.py

---
+++ tests/test_codecs/test_utils/test_refinement.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+import numpy as np
+
+from mmpose.codecs.utils.refinement import refine_keypoints_dark_udp
+
+
+class TestRefineKeypointsDarkUDP(TestCase):
+
+    def test_flat_heatmap_keeps_keypoints_unchanged(self):
+        heatmaps = np.ones((1, 21, 21), dtype=np.float32)
+        keypoints = np.array([[[10.0, 10.0]]], dtype=np.float32)
+
+        refined = refine_keypoints_dark_udp(
+            keypoints.copy(), heatmaps.copy(), blur_kernel_size=11
+        )
+
+        self.assertTrue(np.all(np.isfinite(refined)))
+        np.testing.assert_allclose(refined, keypoints)
+
+    def test_gaussian_heatmap_moves_towards_true_peak(self):
+        height = width = 21
+        y_grid, x_grid = np.mgrid[0:height, 0:width]
+        true_peak = np.array([10.25, 9.75], dtype=np.float32)
+        sigma = 2.0
+
+        heatmaps = np.exp(
+            -((x_grid - true_peak[0]) ** 2 + (y_grid - true_peak[1]) ** 2)
+            / (2 * sigma**2)
+        ).astype(np.float32)[None, ...]
+        keypoints = np.array([[[10.0, 10.0]]], dtype=np.float32)
+
+        refined = refine_keypoints_dark_udp(
+            keypoints.copy(), heatmaps.copy(), blur_kernel_size=11
+        )
+
+        refined_xy = refined[0, 0, :2]
+        initial_xy = keypoints[0, 0, :2]
+
+        self.assertGreater(refined_xy[0], initial_xy[0])
+        self.assertLess(refined_xy[1], initial_xy[1])
+        self.assertLess(
+            np.linalg.norm(refined_xy - true_peak),  # euclidean distance to true peak is less
+            np.linalg.norm(initial_xy - true_peak),
+        )
