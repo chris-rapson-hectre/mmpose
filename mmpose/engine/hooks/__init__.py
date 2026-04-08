fbcf61c0ea966d2cae08ad009a4c2e26ce58be95
Chris Rapson <chris.rapson@hectre.com>
2026-04-08 14:57:15 +1200

    Add Early Stopping Hook

--- mmpose/engine/hooks/__init__.py
+++ mmpose/engine/hooks/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .badcase_hook import BadCaseAnalysisHook
+from .early_stopping_hook import EarlyStoppingHook
 from .ema_hook import ExpMomentumEMA
 from .mode_switch_hooks import RTMOModeSwitchHook, YOLOXPoseModeSwitchHook
 from .sync_norm_hook import SyncNormHook
@@ -9,5 +10,5 @@ from .visualization_hook import PoseVisualizationHook, DebugAugmentedSetupHook
 __all__ = [
     'PoseVisualizationHook', 'ExpMomentumEMA', 'BadCaseAnalysisHook',
     'YOLOXPoseModeSwitchHook', 'SyncNormHook', 'RTMOModeSwitchHook',
-    'UnfreezeBackboneHook', 'DebugAugmentedSetupHook',
+    'UnfreezeBackboneHook', 'DebugAugmentedSetupHook', 'EarlyStoppingHook',
 ]
